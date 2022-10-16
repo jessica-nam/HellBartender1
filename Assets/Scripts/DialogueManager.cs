c89e8c0643a3469509a838bce89886beac877c89
Jessica Nam <jessicanam@nevada.unr.edu>
2022-10-16 04:12:04 -0700

    Initial commit - Bar scene + temp menu scene

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    //keeps track of descendants 
+    private Queue<string> sentences; //FIFO - better format for dialogue
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue){
+        Debug.Log("Starting convo with " + dialogue.name);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(sentences.Count == 0){
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue(){
+        Debug.Log("End convo");
+    }
+}
